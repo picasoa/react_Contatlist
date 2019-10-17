6dc23861f95095abf6bc07bf191baf352eb642f0
Rajat Silwal <rajatsilwal7@gmail.com>
2019-10-17 16:57:10 +0530

    added user list dynamically using map function

--- src/Compo/Section.js
+++ src/Compo/Section.js
@@ -2,6 +2,10 @@ import React,{Component} from 'react';
 
 class Section extends Component{
         render(){
+            let users = [
+                { id:0,name:"rajat",phone:"0909090909"},
+                { id:1,name:"kritika",phone:"0808080808"}
+            ];
         return (
         <div className="section">
         <form>
@@ -10,6 +14,18 @@ class Section extends Component{
             <input type="text" name="name" defaultValue="UserName"/>
             <input type="text" name="phone" defaultValue="888888888"/><br/>
         </form>
+        <table className="subscribers">
+            {
+                users.map(user=>{
+                 return(
+                    <tr key={user.id}>
+                        <td className="name">{user.name}</td>
+                        <td className="phone">{user.phone}</td>
+                    </tr>
+                    )
+                }) 
+            }
+            </table>
         </div>
         );
         }
